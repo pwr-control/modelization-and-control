eaf78fdf3b245d2fd0f47a5a7365b156a29684c2
dbagn <dbagnara@gmail.com>
2025-11-29 19:52:00 +0100

    no message

--- semiconductor_device_modeling/semiconductors/power_mosfet/init_model.m
+++ semiconductor_device_modeling/semiconductors/power_mosfet/init_model.m
@@ -2,7 +2,7 @@ clear all
 close all
 clc
 
-tc = 1e-6;
+tc = 2e-8;
 simlength = 0.2;
 
 % Power semiconductors modelization, IGBT, MOSFET,  and snubber data
@@ -43,7 +43,7 @@ mosfet.inv.Rsnubber = 1;                               % [Ohm]
 % inv.Rsnubber = 1/(mosfet.inv.Csnubber*fPWM_INV)/5
 
 m = 0.5;
-freq = 20e3;
+freq = 35e3;
 L = 500e-6;
 C = 200e-6;
 
