a99889cefbc2d5ff69100e11f581386b9334e43d
dab <dbagnara@gmail.com>
2025-12-13 08:07:31 +0100

    no message

--- power_electronics_design_with_simscape/dcdc_zvs_full_bridge/case_analysis_1/plotting_results.m
+++ power_electronics_design_with_simscape/dcdc_zvs_full_bridge/case_analysis_1/plotting_results.m
@@ -9,7 +9,7 @@ colore2 = [0.5 0.5 0.5];
 colore3 = [0.75 0.75 0.75];
 t1c = time_tc_sim(end) - Nc*tc/1000;
 t2c = time_tc_sim(end);
-t1s = time_ts_dab_sim(end) - Ns_dab*ts_dab/2;
+t1s = time_ts_dab_sim(end) - Ns_dab*ts_dab;
 t2s = time_ts_dab_sim(end);
 t3s = time_ts_dab_sim(end) - Ns_dab*ts_dab/10;
 t4s = time_ts_dab_sim(end);
@@ -96,39 +96,39 @@ yyaxis left;
 plot(time_tc_sim,current_battery_1_sim,'-','LineWidth',tratto1,'Color',colore1);
 ylabel('$i/A$','Interpreter','latex','FontSize', fontsize_plotting);
 hold on
-% set(gca,'ylim',[-800 -200]);
+set(gca,'ylim',[240 250]);
 yyaxis right;
 ax = gca;
 ax.YColor = [0.5 0.5 0.5]; 
 plot(time_tc_sim,voltage_battery_1_sim,'-','LineWidth',tratto2,'Color',colore2);
 ylabel('$u/V$','Interpreter','latex','FontSize', fontsize_plotting);
-% set(gca,'ylim',[1300 1360]);
 hold off
 title('DC input current and voltage','Interpreter','latex','FontSize',fontsize_plotting);
 legend('$i_{1}^{dc}$','$u_{1}^{dc}$','Location','northwestoutside',...
     'Interpreter','latex','FontSize',fontsize_plotting);
 xlabel('$t/s$','Interpreter','latex','FontSize', fontsize_plotting);
-set(gca,'xlim',[t3s t4s]);
+set(gca,'xlim',[t1s t2s]);
+set(gca,'ylim',[760 770]);
 grid on
 subplot 212
 colororder({'k','k'})
 yyaxis left;
-plot(time_tc_sim,current_battery_2_sim,'-','LineWidth',tratto1,'Color',colore1);
+plot(time_tc_sim,-current_battery_2_sim,'-','LineWidth',tratto1,'Color',colore1);
 ylabel('$i/A$','Interpreter','latex','FontSize', fontsize_plotting);
 hold on
-% set(gca,'ylim',[-800 -200]);
+set(gca,'ylim',[295 305]);
 yyaxis right;
 ax = gca;
 ax.YColor = [0.5 0.5 0.5]; 
 plot(time_tc_sim,voltage_battery_2_sim,'-','LineWidth',tratto2,'Color',colore2);
 ylabel('$u/V$','Interpreter','latex','FontSize', fontsize_plotting);
-% set(gca,'ylim',[1300 1360]);
+set(gca,'ylim',[600 610]);
 hold off
 title('DC output current and voltage','Interpreter','latex','FontSize',fontsize_plotting);
 legend('$i_{2}^{dc}$','$u_{2}^{dc}$','Location','northwestoutside',...
     'Interpreter','latex','FontSize',fontsize_plotting);
 xlabel('$t/s$','Interpreter','latex','FontSize', fontsize_plotting);
-set(gca,'xlim',[t3s t4s]);
+set(gca,'xlim',[t1s t2s]);
 grid on
 grid on
 h=gcf;
@@ -141,23 +141,34 @@ movefile('dc_inout_voltage_current.eps', 'figures');
 
 figure(3);
 subplot 211
-plot(time_tc_sim,dab_inverter_1_device_data_sim(:,2),'-','LineWidth',tratto1,'Color',colore1);
+colororder({'k','k'})
+yyaxis left;
+plot(time_tc_sim,full_bridge_inverter_1_device_data_sim(:,2),'-','LineWidth',tratto1,'Color',colore1);
 ylabel('$u/V$','Interpreter','latex','FontSize', fontsize_plotting);
-% set(gca,'ylim',[-50 1000]);
-title('Full-bridge: Q1 voltage','Interpreter','latex','FontSize',fontsize_plotting);
-legend('$u_{Q_1}$','Location','northwestoutside',...
+hold on
+set(gca,'ylim',[-20 850]);
+yyaxis right;
+ax = gca;
+ax.YColor = [0.5 0.5 0.5]; 
+plot(time_tc_sim,full_bridge_inverter_1_device_data_sim(:,21),'-','LineWidth',tratto1,'Color',colore2);
+ylabel('$u/V$','Interpreter','latex','FontSize', fontsize_plotting);
+hold off
+title('Full-bridge: Q1 Uce and Uge','Interpreter','latex','FontSize',fontsize_plotting);
+legend('$u_{ce}^{Q_1}$','$u_{ge}^{Q_1}$','Location','northwestoutside',...
     'Interpreter','latex','FontSize',fontsize_plotting);
-set(gca,'xlim',[t5s t6s]);
+xlabel('$t/s$','Interpreter','latex','FontSize', fontsize_plotting);
+set(gca,'xlim',[t1c t2c]);
+set(gca,'ylim',[-20 25]);
 grid on
 subplot 212
-plot(time_tc_sim,dab_inverter_1_device_data_sim(:,3),'-','LineWidth',tratto1,'Color',colore1);
+plot(time_tc_sim,full_bridge_inverter_1_device_data_sim(:,3),'-','LineWidth',tratto1,'Color',colore1);
 ylabel('$i/A$','Interpreter','latex','FontSize', fontsize_plotting);
 % set(gca,'ylim',[-1600 1000]);
 legend('$i_{Q_1}$','Location','northwestoutside',...
     'Interpreter','latex','FontSize',fontsize_plotting);
 xlabel('$t/s$','Interpreter','latex','FontSize', fontsize_plotting);
 title('Full-bridge: Q1 current','Interpreter','latex','FontSize',fontsize_plotting);
-set(gca,'xlim',[t5s t6s]);
+set(gca,'xlim',[t1c t2c]);
 grid on
 h=gcf;
 set(h,'PaperOrientation','landscape');
@@ -168,23 +179,34 @@ movefile('full_bridge_Q1.eps', 'figures');
 
 figure(4);
 subplot 211
-plot(time_tc_sim,dab_inverter_1_device_data_sim(:,7),'-','LineWidth',tratto1,'Color',colore1);
+colororder({'k','k'})
+yyaxis left;
+plot(time_tc_sim,full_bridge_inverter_1_device_data_sim(:,7),'-','LineWidth',tratto1,'Color',colore1);
 ylabel('$u/V$','Interpreter','latex','FontSize', fontsize_plotting);
-% set(gca,'ylim',[-50 1000]);
-title('Full-bridge: Q2 voltage','Interpreter','latex','FontSize',fontsize_plotting);
-legend('$u_{Q_2}$','Location','northwestoutside',...
+hold on
+set(gca,'ylim',[-20 850]);
+yyaxis right;
+ax = gca;
+ax.YColor = [0.5 0.5 0.5]; 
+plot(time_tc_sim,full_bridge_inverter_1_device_data_sim(:,22),'-','LineWidth',tratto1,'Color',colore2);
+ylabel('$u/V$','Interpreter','latex','FontSize', fontsize_plotting);
+hold off
+title('Full-bridge: Q2 Uce and Uge','Interpreter','latex','FontSize',fontsize_plotting);
+legend('$u_{ce}^{Q_2}$','$u_{ge}^{Q_2}$','Location','northwestoutside',...
     'Interpreter','latex','FontSize',fontsize_plotting);
-set(gca,'xlim',[t5s t6s]);
+xlabel('$t/s$','Interpreter','latex','FontSize', fontsize_plotting);
+set(gca,'xlim',[t1c t2c]);
+set(gca,'ylim',[-20 25]);
 grid on
 subplot 212
-plot(time_tc_sim,dab_inverter_1_device_data_sim(:,8),'-','LineWidth',tratto1,'Color',colore1);
+plot(time_tc_sim,full_bridge_inverter_1_device_data_sim(:,8),'-','LineWidth',tratto1,'Color',colore1);
 ylabel('$i/A$','Interpreter','latex','FontSize', fontsize_plotting);
 % set(gca,'ylim',[-1600 1000]);
 legend('$i_{Q_2}$','Location','northwestoutside',...
     'Interpreter','latex','FontSize',fontsize_plotting);
 xlabel('$t/s$','Interpreter','latex','FontSize', fontsize_plotting);
 title('Full-bridge: Q2 current','Interpreter','latex','FontSize',fontsize_plotting);
-set(gca,'xlim',[t5s t6s]);
+set(gca,'xlim',[t1c t2c]);
 grid on
 h=gcf;
 set(h,'PaperOrientation','landscape');
@@ -196,23 +218,34 @@ movefile('full_bridge_Q2.eps', 'figures');
 
 figure(5);
 subplot 211
-plot(time_tc_sim,dab_inverter_1_device_data_sim(:,12),'-','LineWidth',tratto1,'Color',colore1);
+colororder({'k','k'})
+yyaxis left;
+plot(time_tc_sim,full_bridge_inverter_1_device_data_sim(:,12),'-','LineWidth',tratto1,'Color',colore1);
 ylabel('$u/V$','Interpreter','latex','FontSize', fontsize_plotting);
-% set(gca,'ylim',[-50 1000]);
-title('Full-bridge: Q3 voltage','Interpreter','latex','FontSize',fontsize_plotting);
-legend('$u_{Q_3}$','Location','northwestoutside',...
+hold on
+set(gca,'ylim',[-20 850]);
+yyaxis right;
+ax = gca;
+ax.YColor = [0.5 0.5 0.5]; 
+plot(time_tc_sim,full_bridge_inverter_1_device_data_sim(:,23),'-','LineWidth',tratto1,'Color',colore2);
+ylabel('$u/V$','Interpreter','latex','FontSize', fontsize_plotting);
+hold off
+title('Full-bridge: Q3 Uce and Uge','Interpreter','latex','FontSize',fontsize_plotting);
+legend('$u_{ce}^{Q_3}$','$u_{ge}^{Q_3}$','Location','northwestoutside',...
     'Interpreter','latex','FontSize',fontsize_plotting);
-set(gca,'xlim',[t5s t6s]);
+xlabel('$t/s$','Interpreter','latex','FontSize', fontsize_plotting);
+set(gca,'xlim',[t1c t2c]);
+set(gca,'ylim',[-20 25]);
 grid on
 subplot 212
-plot(time_tc_sim,dab_inverter_1_device_data_sim(:,13),'-','LineWidth',tratto1,'Color',colore1);
+plot(time_tc_sim,full_bridge_inverter_1_device_data_sim(:,13),'-','LineWidth',tratto1,'Color',colore1);
 ylabel('$i/A$','Interpreter','latex','FontSize', fontsize_plotting);
 % set(gca,'ylim',[-1600 1000]);
 legend('$i_{Q_3}$','Location','northwestoutside',...
     'Interpreter','latex','FontSize',fontsize_plotting);
 xlabel('$t/s$','Interpreter','latex','FontSize', fontsize_plotting);
 title('Full-bridge: Q3 current','Interpreter','latex','FontSize',fontsize_plotting);
-set(gca,'xlim',[t5s t6s]);
+set(gca,'xlim',[t1c t2c]);
 grid on
 h=gcf;
 set(h,'PaperOrientation','landscape');
@@ -223,23 +256,34 @@ movefile('full_bridge_Q3.eps', 'figures');
 
 figure(6);
 subplot 211
-plot(time_tc_sim,dab_inverter_1_device_data_sim(:,17),'-','LineWidth',tratto1,'Color',colore1);
+colororder({'k','k'})
+yyaxis left;
+plot(time_tc_sim,full_bridge_inverter_1_device_data_sim(:,17),'-','LineWidth',tratto1,'Color',colore1);
 ylabel('$u/V$','Interpreter','latex','FontSize', fontsize_plotting);
-% set(gca,'ylim',[-50 1000]);
-title('Full-bridge: Q4 voltage','Interpreter','latex','FontSize',fontsize_plotting);
-legend('$u_{Q_4}$','Location','northwestoutside',...
+hold on
+set(gca,'ylim',[-20 850]);
+yyaxis right;
+ax = gca;
+ax.YColor = [0.5 0.5 0.5]; 
+plot(time_tc_sim,full_bridge_inverter_1_device_data_sim(:,24),'-','LineWidth',tratto1,'Color',colore2);
+ylabel('$u/V$','Interpreter','latex','FontSize', fontsize_plotting);
+hold off
+title('Full-bridge: Q4 Uce and Uge','Interpreter','latex','FontSize',fontsize_plotting);
+legend('$u_{ce}^{Q_4}$','$u_{ge}^{Q_4}$','Location','northwestoutside',...
     'Interpreter','latex','FontSize',fontsize_plotting);
-set(gca,'xlim',[t5s t6s]);
+xlabel('$t/s$','Interpreter','latex','FontSize', fontsize_plotting);
+set(gca,'xlim',[t1c t2c]);
+set(gca,'ylim',[-20 25]);
 grid on
 subplot 212
-plot(time_tc_sim,dab_inverter_1_device_data_sim(:,18),'-','LineWidth',tratto1,'Color',colore1);
+plot(time_tc_sim,full_bridge_inverter_1_device_data_sim(:,18),'-','LineWidth',tratto1,'Color',colore1);
 ylabel('$i/A$','Interpreter','latex','FontSize', fontsize_plotting);
 % set(gca,'ylim',[-1600 1000]);
 legend('$i_{Q_4}$','Location','northwestoutside',...
     'Interpreter','latex','FontSize',fontsize_plotting);
 xlabel('$t/s$','Interpreter','latex','FontSize', fontsize_plotting);
 title('Full-bridge: Q4 current','Interpreter','latex','FontSize',fontsize_plotting);
-set(gca,'xlim',[t5s t6s]);
+set(gca,'xlim',[t1c t2c]);
 grid on
 h=gcf;
 set(h,'PaperOrientation','landscape');
@@ -249,92 +293,53 @@ print('full_bridge_Q4','-depsc');
 movefile('full_bridge_Q4.eps', 'figures');
 
 
-figure(9);
+figure(7);
 subplot 211
 yyaxis left;
 ax = gca;
-ax.YColor = [0.25 0.25 0.25]; 
-plot(time_tc_sim,dab_inverter_1_device_data_sim(:,1),'-','LineWidth',tratto1,'Color',colore1);
+ax.YColor = [0 0 0]; 
+plot(time_tc_sim,full_bridge_inverter_1_device_data_sim(:,1),'-','LineWidth',tratto1,'Color',colore1);
 ylabel('$p/W$','Interpreter','latex','FontSize', fontsize_plotting);
 xlabel('$t/s$','Interpreter','latex','FontSize', fontsize_plotting);
-set(gca,'xlim',[t5c t6c]);
+% set(gca,'ylim',[500 700]);
 yyaxis right;
 ax = gca;
 ax.YColor = [0.5 0.5 0.5]; 
-plot(time_tc_sim,dab_inverter_1_device_data_sim(:,6),'-','LineWidth',tratto1,'Color',colore1);
-ylabel('$i/A$','Interpreter','latex','FontSize', fontsize_plotting);
-set(gca,'ylim',[-200 1000]);
-grid on
-legend('$u_{Q_1}$','$i_{Q_1}$','Location','northwestoutside',...
-    'Interpreter','latex','FontSize',fontsize_plotting);
-set(gca,'xlim',[t5c t6c]);
-title('DAB secondary side: Q1 voltage and current','Interpreter','latex','FontSize',fontsize_plotting);
-grid on
-subplot 212
-yyaxis left;
-ax = gca;
-ax.YColor = [0.25 0.25 0.25]; 
-plot(time_tc_sim,dab_inverter_1_device_data_sim(:,1),'-','LineWidth',tratto1,'Color',colore1);
-ylabel('$u/V$','Interpreter','latex','FontSize', fontsize_plotting);
-xlabel('$t/s$','Interpreter','latex','FontSize', fontsize_plotting);
-set(gca,'ylim',[-50 1750]);
-xlabel('$t/s$','Interpreter','latex','FontSize', fontsize_plotting);
-set(gca,'xlim',[t5c t6c]);
-yyaxis right;
-ax = gca;
-ax.YColor = [0.5 0.5 0.5]; 
-plot(time_tc_sim,inverter_2_dab_devices_data_modA_sim(:,3),'-','LineWidth',tratto1,'Color',colore2);
-ylabel('$i/A$','Interpreter','latex','FontSize', fontsize_plotting);
-set(gca,'ylim',[-200 1000]);
-grid on
-legend('$u_{Q_1}$','$i_{Q_1}$','Location','northwestoutside',...
-    'Interpreter','latex','FontSize',fontsize_plotting);
-set(gca,'xlim',[t5c t6c]);
-title('DAB secondary side: Q1 voltage and current','Interpreter','latex','FontSize',fontsize_plotting);
-grid on
-h=gcf;
-set(h,'PaperOrientation','landscape');
-set(h,'PaperUnits','normalized');
-set(h,'PaperPosition', [0 0 1 1]);
-print('DAB_secondary_side_Q1','-depsc');
-movefile('DAB_secondary_side_Q1.eps', 'figures')
-
-
-figure(10);
-subplot 211
-plot(time_tc_sim,inverter_2_dab_devices_data_modA_sim(:,6),'-','LineWidth',tratto1,'Color',colore1);
+plot(time_tc_sim,full_bridge_inverter_1_device_data_sim(:,6),'-','LineWidth',tratto1,'Color',colore2);
 ylabel('$p/W$','Interpreter','latex','FontSize', fontsize_plotting);
-set(gca,'ylim',[320 330]);
+grid on
+legend('$p_{Q_1}$','$p_{Q_1}$','Location','northwestoutside',...
+    'Interpreter','latex','FontSize',fontsize_plotting);
 set(gca,'xlim',[t1s t2s]);
-title('DAB secondary side: Q2 power loss','Interpreter','latex','FontSize',fontsize_plotting);
-legend('$p_{Q_2}$','Location','northwestoutside',...
-    'Interpreter','latex','FontSize',fontsize_plotting);
+% set(gca,'ylim',[500 700]);
+title('Full-bridge: Q1/Q2 power loss','Interpreter','latex','FontSize',fontsize_plotting);
 grid on
 subplot 212
 yyaxis left;
 ax = gca;
-ax.YColor = [0.25 0.25 0.25]; 
-plot(time_tc_sim,inverter_2_dab_devices_data_modA_sim(:,7),'-','LineWidth',tratto1,'Color',colore1);
-ylabel('$u/V$','Interpreter','latex','FontSize', fontsize_plotting);
-set(gca,'ylim',[-50 1750]);
+ax.YColor = [0 0 0]; 
+plot(time_tc_sim,full_bridge_inverter_1_device_data_sim(:,11),'-','LineWidth',tratto1,'Color',colore1);
+ylabel('$p/W$','Interpreter','latex','FontSize', fontsize_plotting);
 xlabel('$t/s$','Interpreter','latex','FontSize', fontsize_plotting);
-set(gca,'xlim',[t5c t6c]);
+% set(gca,'ylim',[500 700]);
+set(gca,'xlim',[t1s t2s]);
 yyaxis right;
 ax = gca;
 ax.YColor = [0.5 0.5 0.5]; 
-plot(time_tc_sim,inverter_2_dab_devices_data_modA_sim(:,8),'-','LineWidth',tratto1,'Color',colore2);
-ylabel('$i/A$','Interpreter','latex','FontSize', fontsize_plotting);
-set(gca,'ylim',[-200 1000]);
+plot(time_tc_sim,full_bridge_inverter_1_device_data_sim(:,16),'-','LineWidth',tratto1,'Color',colore2);
+ylabel('$p/W$','Interpreter','latex','FontSize', fontsize_plotting);
 grid on
-legend('$u_{Q_2}$','$i_{Q_2}$','Location','northwestoutside',...
+legend('$p_{Q_3}$','$p_{Q_4}$','Location','northwestoutside',...
     'Interpreter','latex','FontSize',fontsize_plotting);
-xlabel('$t/s$','Interpreter','latex','FontSize', fontsize_plotting);
-set(gca,'xlim',[t5c t6c]);
-title('DAB secondary side: Q2 voltage and current','Interpreter','latex','FontSize',fontsize_plotting);
+set(gca,'xlim',[t1s t2s]);
+% set(gca,'ylim',[500 700]);
+title('Full-bridge: Q3/Q4 power loss','Interpreter','latex','FontSize',fontsize_plotting);
 grid on
 h=gcf;
 set(h,'PaperOrientation','landscape');
 set(h,'PaperUnits','normalized');
 set(h,'PaperPosition', [0 0 1 1]);
-print('DAB_secondary_side_Q2','-depsc');
-movefile('DAB_secondary_side_Q2.eps', 'figures')
+print('full_bridge_device_losses','-depsc');
+movefile('full_bridge_device_losses.eps', 'figures')
+
+
