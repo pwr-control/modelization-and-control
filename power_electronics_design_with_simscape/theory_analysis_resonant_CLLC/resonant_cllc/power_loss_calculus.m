cb25010ec8394484bfa4e2d975d4c64bcfe7c032
dab <dbagnara@gmail.com>
2025-12-30 16:29:37 +0100

    tc affecting power balance (power lost somewhere) in CLLC, set a smaller tc

--- power_electronics_design_with_simscape/theory_analysis_resonant_CLLC/resonant_cllc/power_loss_calculus.m
+++ power_electronics_design_with_simscape/theory_analysis_resonant_CLLC/resonant_cllc/power_loss_calculus.m
@@ -13,7 +13,17 @@ udc_2 = mean(voltage_battery_2_sim)
 p1 = idc_1*udc_1
 p2 = idc_2*udc_2
 
-ploss_total = p1+p2
+
+i1_tr = sqrt(mean(i1_cllc_transformer_modA_sim.^2))
+i2_tr = sqrt(mean(i2_cllc_transformer_modA_sim.^2))
+u1_tr = sqrt(mean(u1_cllc_transformer_modA_sim.^2))
+u2_tr = sqrt(mean(u2_cllc_transformer_modA_sim.^2))
+
+p1_tr = mean(i1_cllc_transformer_modA_sim.*u1_cllc_transformer_modA_sim)
+p2_tr = mean(i2_cllc_transformer_modA_sim.*u2_cllc_transformer_modA_sim)
+
+p_balance_dc = p1 + p2
+p_balance_ac = p1_tr - p2_tr
 
 p_loss_cllc_1_Q1 = mean(inverter_1_cllc_devices_data_modA_sim(:,1))
 p_loss_cllc_1_Q2 = mean(inverter_1_cllc_devices_data_modA_sim(:,6))
@@ -28,8 +38,3 @@ p_loss_cllc = (p_loss_cllc_1_Q1 + p_loss_cllc_1_Q2 + ...
     p_loss_cllc_1_Q3 + p_loss_cllc_1_Q4) + ...
     (p_loss_cllc_2_Q1 + p_loss_cllc_2_Q2 + ...
     p_loss_cllc_2_Q3 + p_loss_cllc_2_Q4)
-
-i1_tr = sqrt(mean(i1_cllc_transformer_modA_sim.^2))
-i2_tr = sqrt(mean(i2_cllc_transformer_modA_sim.^2))
-u1_tr = sqrt(mean(u1_cllc_transformer_modA_sim.^2))
-u2_tr = sqrt(mean(u2_cllc_transformer_modA_sim.^2))
