5108ba3c2ffc881160dcee325563862d7e32e217
dab <dbagnara@gmail.com>
2026-01-06 20:51:15 +0100

    no message

---
+++ power_electronics_design_with_simscape/theory_analysis_resonant_CLLC/resonant_cllc/cllc_analysis.m
@@ -0,0 +1,29 @@
+
+Rload = 10;
+
+ZLs1 = s*Ls1;
+ZCs1 = 1/s/Cs1;
+
+ZLm = s*lm_trafo;
+
+ZLs2 = s*Ls2;
+ZCs2 = 1/s/Cs2;
+
+ZLout = ZLs2 + ZCs2 + Rload;
+
+Zpout = minreal((ZLout + ZLm)/(ZLout * ZLm));
+Ht = minreal(Zpout / (ZCs1 + ZLs1 + Zpout) / ZLout * Rload);
+figure; bode(Ht,options); grid on
+set(gca,'xlim', [fPWM_CLLC/4 fPWM_CLLC*4])
+complex_response = evalfr(Ht, 1i*2*pi*fPWM_CLLC);
+
+% 4. Display the result
+disp('Complex Response H(jw):');
+disp(complex_response);
+
+% 5. Extract Magnitude and Phase
+magnitude = abs(complex_response);
+phase_deg = rad2deg(angle(complex_response));
+
+fprintf('Magnitude: %.4f\n', magnitude);
+fprintf('Phase: %.2f degrees\n', phase_deg);
