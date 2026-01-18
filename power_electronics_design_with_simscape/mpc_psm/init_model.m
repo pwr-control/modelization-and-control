7500fb14ef1533dd47ff6e1b79a142c56bd7313a
dab <dbagnara@gmail.com>
2026-01-18 18:44:15 +0100

    modelization on dc-supplied inverter for motor control analysis (zero speed sensorless control with HF injection) and modulation strategies (mpc)

--- power_electronics_design_with_simscape/mpc_psm/init_model.m
+++ power_electronics_design_with_simscape/mpc_psm/init_model.m
@@ -159,8 +159,8 @@ omega_rso = 2*pi*50;
 p2place = [-1 -4]*omega_rso;
 p2placed = exp(p2place*ts_afe);
 Kd = (acker(Asod',Cso',p2placed))';
-l1 = Kd(2) %[output:48227209]
-l2 = Kd(1) %[output:3070f48b]
+l1 = Kd(2) %[output:8bf67124]
+l2 = Kd(1) %[output:45e82fc7]
 
 %[text] #### Grid fault generator 
 grid_fault_generator;
@@ -239,7 +239,7 @@ polesrso = [-1 -4]*omega_rso;
 Lrso = acker(Arso',Crso',polesrso)';
 Adrso = eye(2) + Arso*ts_afe;
 polesdrso = exp(ts_afe*polesrso);
-Ldrso = acker(Adrso',Crso',polesdrso)' %[output:3c155842]
+Ldrso = acker(Adrso',Crso',polesdrso)' %[output:3873a036]
 
 %[text] ### PLL DDSRF
 pll_i1_ddsrt = pll_i1/2;
@@ -250,13 +250,13 @@ ddsrf_fd = c2d(ddsrf_f,ts_afe);
 %%
 %[text] ### First Harmonic Tracker for Ugrid cleaning
 omega0 = 2*pi*50;
-Afht = [0 1; -omega0^2 -0.05*omega0] % impianto nel continuo %[output:89f0f782]
+Afht = [0 1; -omega0^2 -0.05*omega0] % impianto nel continuo %[output:2f8df18e]
 Cfht = [1 0];
 poles_fht = [-1 -4]*omega0;
-Lfht = acker(Afht',Cfht',poles_fht)' % guadagni osservatore nel continuo %[output:9a38523f]
-Ad_fht = eye(2) + Afht*ts_afe % impianto nel discreto %[output:45969b88]
+Lfht = acker(Afht',Cfht',poles_fht)' % guadagni osservatore nel continuo %[output:45ef9943]
+Ad_fht = eye(2) + Afht*ts_afe % impianto nel discreto %[output:20f05bf9]
 polesd_fht = exp(ts_afe*poles_fht);
-Ld_fht = Lfht*ts_afe % guadagni osservatore nel discreto %[output:36e3e7d0]
+Ld_fht = Lfht*ts_afe % guadagni osservatore nel discreto %[output:0445503e]
 %[text] ### Reactive current control gains
 kp_rc_grid = 0.35;
 ki_rc_grid = 35;
@@ -365,8 +365,8 @@ Cso = [1 0];
 % p2place = exp([-10 -50]*ts_inv);
 p2place = exp([-50 -250]*ts_inv);
 Kobs = (acker(Aso',Cso',p2place))';
-kg = Kobs(1) %[output:983bdd5b]
-kw = Kobs(2) %[output:2c5ea1cd]
+kg = Kobs(1) %[output:04a70309]
+kw = Kobs(2) %[output:69e958f5]
 
 %[text] ### Rotor speed observer with load estimator
 A = [0 1 0; 0 0 -1/Jm_norm; 0 0 0];
@@ -375,9 +375,9 @@ Blo = [0; ts_inv/Jm_norm; 0];
 Clo = [1 0 0];
 p3place = exp([-1 -5 -25]*125*ts_inv);
 Klo = (acker(Alo',Clo',p3place))';
-luenberger_l1 = Klo(1) %[output:9cd1b426]
-luenberger_l2 = Klo(2) %[output:0f86fa0a]
-luenberger_l3 = Klo(3) %[output:52a550a8]
+luenberger_l1 = Klo(1) %[output:8e3d2c7e]
+luenberger_l2 = Klo(2) %[output:95bd65ef]
+luenberger_l3 = Klo(3) %[output:7d37a44a]
 omega_flt_fcut = 10;
 % phase_compensation_omega = -pi/2-pi/12; % for motor mode
 phase_compensation_omega = 0; % for generator mode
@@ -419,12 +419,10 @@ motorc_m_scale = 2/3*Vdc_bez/ubez;
 %%
 %[text] ## Power semiconductors modelization, IGBT, MOSFET,  and snubber data
 %[text] ### HeatSink settings
-heatsink_liquid_2kW; %[output:7700a89a] %[output:36462aac] %[output:57299408]
+heatsink_liquid_2kW;
 %[text] ### DEVICES settings (IGBT)
-% infineon_FF650R17IE4D_B2;
-infineon_FF1200R17IP5;
-% danfoss_DP650B1700T104001;
-% infineon_FF1200XTR17T2P5;
+igbt.data = 'infineon_FF1200R17IP5';
+run(igbt.data);
 
 igbt.inv.Vth = Vth;                                  % [V]
 igbt.inv.Vce_sat = Vce_sat;                          % [V]
@@ -441,17 +439,16 @@ igbt.inv.Rtim = Rtim;                                % [K/W]
 igbt.inv.Rth_switch_JC = Rth_switch_JC;              % [K/W]
 igbt.inv.Rth_switch_CH = Rth_switch_CH;              % [K/W]
 igbt.inv.Rth_switch_JH = Rth_switch_JH;              % [K/W]
-igbt.inv.Lstray_module = Lstray_module;              % [H]
-igbt.inv.Irr = Irr;                                  % [A]
-igbt.inv.Csnubber = Csnubber;                        % [F]
-igbt.inv.Rsnubber = Rsnubber;                        % [Ohm]
+igbt.inv.Rth_diode_JC = Rth_diode_JC;              % [K/W]
+igbt.inv.Rth_diode_CH = Rth_diode_CH;              % [K/W]
+igbt.inv.Rth_diode_JH = Rth_diode_JH;              % [K/W]
+igbt.inv.Lstray_module = Lstray_module;            % [H]
+igbt.inv.Irr = Irr;                                % [A]
+igbt.inv.Cies = Cies;                              % [F]
+igbt.inv.Csnubber = Csnubber;                      % [F]
+igbt.inv.Rsnubber = Rsnubber;                      % [Ohm]
 % inv.Csnubber = (inv.Irr)^2*Lstray_module/Vdc_bez^2
 % inv.Rsnubber = 1/(inv.Csnubber*fPWM_INV)/5
-
-% infineon_FF650R17IE4;
-infineon_FF1200R17IP5;
-% danfoss_DP650B1700T104001;
-% infineon_FF1200XTR17T2P5;
 
 igbt.afe.Vth = Vth;                                  % [V]
 igbt.afe.Vce_sat = Vce_sat;                          % [V]
@@ -470,13 +467,15 @@ igbt.afe.Rth_switch_CH = Rth_switch_CH;              % [K/W]
 igbt.afe.Rth_switch_JH = Rth_switch_JH;              % [K/W]
 igbt.afe.Lstray_module = Lstray_module;              % [H]
 igbt.afe.Irr = Irr;                                  % [A]
+igbt.afe.Cies = Cies;                                % [F]
 igbt.afe.Csnubber = Csnubber;                        % [F]
 igbt.afe.Rsnubber = Rsnubber;                        % [Ohm]
 % afe.Csnubber = (afe.Irr)^2*Lstray_module/Vdc_bez^2
 % afe.Rsnubber = 1/(afe.Csnubber*fPWM_AFE)/5
 
 %[text] ### DEVICES settings (MOSFET)
-infineon_FF1000UXTR23T2M1;
+mosfet.data = 'danfoss_SKM1700MB20R4S2I4'
+run(mosfet.data);
 
 mosfet.inv.Vth = Vth;                                  % [V]
 mosfet.inv.Rds_on = Rds_on;                            % [V]
@@ -579,12 +578,12 @@ rKalman = 1;
 Zmodel = (0:1e-3:1);
 ocv_model = E_1*exp(-Zmodel*alpha) + E0 + E1*Zmodel + E2*Zmodel.^2 +...
     E3*Zmodel.^3 + Elog*log(1-Zmodel+ts_inv);
-figure;  %[output:51db43ea]
-plot(Zmodel,ocv_model,'LineWidth',2); %[output:51db43ea]
-xlabel('state of charge [p.u.]'); %[output:51db43ea]
-ylabel('open circuit voltage [V]'); %[output:51db43ea]
-title('open circuit voltage(state of charge)'); %[output:51db43ea]
-grid on %[output:51db43ea]
+figure;  %[output:339abfd4]
+plot(Zmodel,ocv_model,'LineWidth',2); %[output:339abfd4]
+xlabel('state of charge [p.u.]'); %[output:339abfd4]
+ylabel('open circuit voltage [V]'); %[output:339abfd4]
+title('open circuit voltage(state of charge)'); %[output:339abfd4]
+grid on %[output:339abfd4]
 %[text] ## C-Caller Settings
 open_system(model);
 Simulink.importExternalCTypes(model,'Names',{'mavgflt_output_t'});
@@ -634,51 +633,42 @@ end
 %[output:7e5df9b4]
 %   data: {"dataType":"textualVariable","outputData":{"name":"deepNEGeta","value":"   0.500000000000000"}}
 %---
-%[output:48227209]
+%[output:8bf67124]
 %   data: {"dataType":"textualVariable","outputData":{"name":"l1","value":"  15.921682195383369"}}
 %---
-%[output:3070f48b]
+%[output:45e82fc7]
 %   data: {"dataType":"textualVariable","outputData":{"name":"l2","value":"   0.064017382188212"}}
 %---
-%[output:3c155842]
+%[output:3873a036]
 %   data: {"dataType":"matrix","outputData":{"columns":1,"name":"Ldrso","rows":2,"type":"double","value":[["0.064017382188212"],["15.921682195383369"]]}}
 %---
-%[output:89f0f782]
+%[output:2f8df18e]
 %   data: {"dataType":"matrix","outputData":{"columns":2,"exponent":"4","name":"Afht","rows":2,"type":"double","value":[["0","0.000100000000000"],["-9.869604401089358","-0.001570796326795"]]}}
 %---
-%[output:9a38523f]
+%[output:45ef9943]
 %   data: {"dataType":"matrix","outputData":{"columns":1,"exponent":"5","name":"Lfht","rows":2,"type":"double","value":[["0.015550883635269"],["2.716608611399846"]]}}
 %---
-%[output:45969b88]
+%[output:20f05bf9]
 %   data: {"dataType":"matrix","outputData":{"columns":2,"name":"Ad_fht","rows":2,"type":"double","value":[["1.000000000000000","0.000041666666667"],["-4.112335167120566","0.999345501530502"]]}}
 %---
-%[output:36e3e7d0]
+%[output:0445503e]
 %   data: {"dataType":"matrix","outputData":{"columns":1,"name":"Ld_fht","rows":2,"type":"double","value":[["0.064795348480289"],["11.319202547499357"]]}}
 %---
-%[output:983bdd5b]
+%[output:04a70309]
 %   data: {"dataType":"textualVariable","outputData":{"name":"kg","value":"   0.012443765785704"}}
 %---
-%[output:2c5ea1cd]
+%[output:69e958f5]
 %   data: {"dataType":"textualVariable","outputData":{"name":"kw","value":"   0.517590710054527"}}
 %---
-%[output:9cd1b426]
+%[output:8e3d2c7e]
 %   data: {"dataType":"textualVariable","outputData":{"name":"luenberger_l1","value":"   0.152987786896029"}}
 %---
-%[output:0f86fa0a]
+%[output:95bd65ef]
 %   data: {"dataType":"textualVariable","outputData":{"name":"luenberger_l2","value":"  93.745795204429072"}}
 %---
-%[output:52a550a8]
+%[output:7d37a44a]
 %   data: {"dataType":"textualVariable","outputData":{"name":"luenberger_l3","value":"    -1.166194503484206e+02"}}
 %---
-%[output:7700a89a]
-%   data: {"dataType":"textualVariable","outputData":{"name":"heat_capacity","value":"  13.199999999999999"}}
-%---
-%[output:36462aac]
-%   data: {"dataType":"textualVariable","outputData":{"name":"Rth_switch_HA","value":"   0.007500000000000"}}
-%---
-%[output:57299408]
-%   data: {"dataType":"textualVariable","outputData":{"name":"Rth_mosfet_HA","value":"   0.007500000000000"}}
-%---
-%[output:51db43ea]
+%[output:339abfd4]
 %   data: {"dataType":"image","outputData":{"dataUri":"data:image\/png;base64,iVBORw0KGgoAAAANSUhEUgAAAN4AAACGCAYAAACyujQtAAAAAXNSR0IArs4c6QAAGKNJREFUeF7tXX9s1dd1P2lphcmIgoEkIsQxSXBStSkjbGvmsQWWJvAPZLU6OUYCBpRRBlisuGB+RARtGMNwpwGtwiLXgmkxKUmU4GkTS8mMknhUSwBLE0lNA8YNjIQYUCIgU7t4Ol\/nvJx3fe\/33e\/3e+\/3ved3vlJF83x\/nc89n3vOPffXTQMDAwOXL1+GJUuWQHd3N+T6pkyZAq2trVBeXp4rqfxdEBAEDAjcRMTbsmULbN68OZRQSFCbdIK2ICAIhCMQEE9AEgQEgXQRyFg8dDXxK2Y38tixYzBv3jx47rnn4OGHH3aKpFr2jRs3YNeuXbB06VJnbjeW2djYGLS7ubkZysrKAOs9d+4c1NbWOpVHLez555+Hrq6uTL1hle3ZswdmzZoFkydPztmmKGlzFvZ5AsKpo6MDJkyYAG1tbVZt0eFrW6fLdOg5ZiweFwYriSKQy0YVS1nbt28PSOFyoFIV4\/3334dFixbBqlWrvBKP5vhPPvlkznqQoLt377ZS9ihpo\/T76dOnA1zmzJkD69ats85aKMTDBhtdTQRt\/fr1GaG2bdsWdAoJfeHCheBvZF1IqI8\/\/jj4\/ejRo0PIS1YD\/86DNPT7U089FSgylk316VA1tYFbJSwfrQe1B\/PgyDhx4sTgdxwt8Vu2bFnQeVQmKblq4fh\/owUibNQBSte5KklzYYjtWrt2LaxcuTIT8KJ28n5R68Z69u7dG+A+Y8YM6OzszBBEHVg5vipB1GAbpeX9R31fVVWVFZijdurSohfC24\/EIcuu9rOpDervujJUWalNOh1Vg4WEoUlHsSzEmMoMw1xtK\/fErOZ4FFSZP38+rF69OjMC8w4jhX777beDzh47dmzQIRUVFQG4fPSeO3dulkuF0VR0EbkwJmuijs5cqXt6ejKuJhGP2kNuEaZHYmObqF4EBNvLrUsY8VCBwiwe4nLgwIFgEMEPccA8OoLrMCRXU7V4SNimpiZoaWnJlEv4kixIEsKXy27CiWQhTNC95WlfffXVLAunkhTTVlZWBoMykYoUTE3LMSXCEi6ceNTH9De1L3JZPFMfqzqBdap93t7enqWvZFWpDaSjmJd+02FOfKC+PHToUBaOWuKpTKWR9fjx41mZOUD19fVD5icceDUvV04iDI2sYS4KCtvQ0KB1dXQWjwRHhQpzqaJYvFzEo7J27twZ6BOfd5qsC5apYmhyNXVWA+ebaMVpvsProUGQFJnjoA6CRDwc1XVLR7q+MY3sOpLyAZUTBOulTzef5nNfwkXnaob1MVm8vr4+7aBI9XOrp1pwbrUwnQlzldRcJ7AfsoIrtI5nmt+po3wU4r300kuBieYf1dPf3x+qnDxPLlKSktPoxomnkouX65J41MEoH42MNBeMgqFKPFJIVDhc+sGlHZIPiceVmuNESkDTA5IblQqDQ9wzQQKorjAnoM5Co\/KhQoYNMqqLT22wIbdp7qsjXlgfq+Xgf3NvhAY0jovJ6mL71b7k2JBOqy40GZeAeNgA\/F+uRfEoozV2YJjF4w1SRzhbcqmRS1uLp3NvXBIPZSMZxo8fn3Ez+e9kmcIGL5V4vKMRX24Folg8jn1YwIHPlfgck4IrOlctzLrbBqRcWDxdH4cRT\/U2VFLqIuZRLJ5KwEgL6G+99RYsWLAgcPUWL16cRSw+ouHoRz58lDmeaW7AG636\/7pRBsvRWTx1lMJRiXz8xx57LGv0I3eD2qQCn0uJuNXg7olK8ChzPJ2sFFzgczyS5dKlSxnXM9ccj6ylSmiV\/CYXlhSXrB8FUngENM05HsnD+1h1q1VyqXNbDKCFuZp8jqdibjXHi7JlDAmFkcKLFy9mRS35aDJ69OjA9VDdiFxRTRviIXmiRDW5q4n\/3xTxImtE0UqKXpmIx2XRrRuq8wnuTfD2c7deHZGxTRSBRYLxSCfmQ2uKH3djfUQ1eeSQtx3dJvwIM+xvJDtFhtW0PACD+aJENXWDl2k5IVdUk3RCJZ7aL4ivGrxS+9prVFM1k7r\/RiFWrFgBJ06cgP3798ODDz6YlUwFQ\/Z82qBqnyaqRdZ5ERh1db3xwF6C4kuZBHOr5QQbSNBqoinHf9EdUYmHv69ZswY2bNhgtcvApk5J8wUC6sCGf4mygwf7zHbniuA+iEBczLN2riQFEzsO3QxcO9KRi69B5QriJG2L5BcECh0BJxYPSbVv3z5Yvnw5bNq0SUs8vvaEoITtTCl00KR9gkBSBBITD83t1q1bYeHChcHuDxt3koI5MqdI2n2Sv1gRSEw8NcKIQOTaYE2+cXV1ddam3HvuuadYcZR2D1MEjhw5ApMmTXIu3RDi8QkjhbBN7qPamrAACrqa+NFGawyN79ixIyvQgsQ7c+aMcyFLrUDB0V2P+8Iyi3h8q1NNTU0wb9u4cSPgBk+biJdKPE62sN3xBJMvId11Q3GUdPbsWS+jdHFI77aVvnQyi3j8agdc8SfiIWnSuPLBl5Buu6LwSxPiuesjXzo5xNWkHeO4JezgwYNBpBLPhen2vrkTb7AkX0K6bmehlyfEc9dDvnRSG1xRDzGmFfr3JaS7biiOkoR47vrJl04mjmq6E1EsnisshXiukPSnk0I8d31UMCUJ8dx1RSoWL9cphVzrc0nF9SVk0nYVW34hnrse86WTQyweLgH09vZm3d5Evz3yyCNAd1Lwo\/quxPQlpKv2FUs5Qjx3PeVLJ43LCXwjMy0z4GFHPOmc68bpuGL7EjJue4o1nxDPXc\/50kntAjq\/nYq2hE2bNg2eeOIJeOWVV6wuPY0jui8h47SlmPMI8dz1ni+dtFpOoKscbDZAJxHZl5BJ2lSMeYV47npt4oz58H7nP7kr8POSJKrpHNL8FyjEc9MH7f91EVa0vwOXfzTTTYGsFCGec0jzX6AQz00fpEo89cAqiZDGHSniarpRGCGeGxxTIx4\/XYAX0OLyAd2aTNd0uxFJX4oQzw26Qjw3OKZKPDqFgHfm03qe6wcpcSM2fupLL0I8NwojxHOD4\/bDvbD98Fn\/czx+Mvyhhx4K7nHEw6r47gE9wpH0oiLd4w8EkxDPjcII8dzgmBrxsLncuqHVo8tKo1wVZxKbysaXe65duyYWz41+DClFiOcG2FSJ56bJ+lLQxcR5I74vp25Lwxxi8dygL8RzgyMuJeA8z\/tygmku52KOhy4mPlaJ8zrdflAiHl4uI18yBPC9A7y7X75kCHxr82H47bj7\/REv16kEbH7YPfc24vE3xyg9f+RPLJ4NinZpxOLZ4RSWqu\/yp\/C7f\/ufQZK8WbzkYmSXEGbx5Jax5GgL8ZJj2NlzBWqeOZkO8ZI3164EIZ4dTnFTCfHiIjeY741fXYW5PzkR\/P8vXf8IPnrmz5MVqMmtfRFWV4vsXHGOvbcChXjxof2H1\/pgy7+8lyng1peXeLnrVfZqxu+jgs0pxIveNat\/9kvYf+xCJmNF+UjY8+TXYMHjDwnxosNZmjmEeLn7HYMnP+06D7te6xuSGEl36K+mAv7ra4kr9Ap3alHSiGZuGAZT+BLStv7hkk6IN7QnkWgvn\/wQfv5OP7zx3lVtV3PCUQJfOmm8wp3vo6RLbpubm8HHXSu+hRwuhLKVQ4gHsOrAu3Cu\/4aRZIQlku3A974JD9xxsxbeVIjncwHdRml8CWlT93BKUwrEQwt2\/uqnsO3fzgZdZ7Jiar8i0WZUlcMPvn134Erm+nzppPYK946ODmhrawte8qE7V0wPvedqeJS\/+xIyShuGQ9rhQDwk1gAA7HrtHJz+4Dr0XfkU8LcoHxJr+r1jYE\/dA0FeG6Kp5fvSSW1UM1+vt\/oSMkpnDYe0xUC8gQGAv3u1F944fSWSxdJZMPxt+n1jYPWfVsB9t41y2oW+dFKWE5x2U2EUlg\/icWv05ntXAf\/X138jlqXSkatizMiAVKtmVsCkcWWpAS3ESw3q4q\/IJfGIUFeu\/wb2HbsAv\/rgeiILZbJYSKw\/njwGfvh4ZUF1QCrEo83SFRUV3u7ODEPVl5AF1ZMpNMaGeJ8NAPzzL\/4Hjp29Cr++\/KkTy2SyVHeVj4SaqbfDow+UpyC92yp86aTVOp56isCtaF+U5ktIX+0thHLJIn02MABH3r0MJ3\/9Cbx7\/gpcugGRgxE28lCAAi1U5bgyqP29O+CuMYPRwTjBC5s685nGl05azfHwLB2u5bW2tkLSqx\/E4pkR4POkC1f\/F1448QH0XLwWZIgT1bNVWE4mnD8t+qM7oXzUV4YtmWxxwXSpEU99qxwrz\/VKEH\/I0rTLRS1Xt+nal5BRgPaR9ujpK\/DyiQ\/hvUuD8yOfJAr665YRMGLECECrNPn2UTD76+Pg\/tu\/WCAejpbJR7+lRjya42GFttaNXwmIp54bGxuhuroaamtrs7Dg6XB9UPcVIvG4FfrN\/30Gh0\/1w7sXr0HvRzdSIRG3RljhtLtvge\/\/yV1w+y1fNeqazRzPl6IOt3J96aSVq2kLJlm1urq64D5O\/uFCfFNTE7S0tBjdVV9Cqu0nMuG\/Lxz\/AM6kZIlUEmF4\/DtTb4O7y78Ij7uwRkI8W43Nnc6XTjojHrmbJlfTZlHepZBIqt8Z+WX4i7b\/9uLaqUGG+8aPgllfHwtlX\/lypjddkCi3agxNIcSLg5o+j0ud5DU4Ix4VigTr6uoKXY4glxY3YnPLmFRInEP99c9+ab1vj9rMSXT32DJ4pGpMEP7+0k3uOjDNkoR47tBOqpOmljgnnk0ElF+cy+eCKGScW8b+8qWL8PZ5\/T4+DDTg93BFGcyfeguM+JxN9Lu7LiqckuSWMTd98eijjwYF+bgHKNKLsLqXYNUTDabr2dES4odEw\/ke3VLNAy1RR5e\/\/\/k5+Jt\/PZOFMlqvud8cD9+bPnFYrivZqJRYPBuU7NJE1Um7UgGs71wJOwxrWk7gZFOXE7Zt2zYk8mkrJM7f8DIaHnHUHWK0BWG4pRPiuetRW52MWqOVxYtaaNz0NkLy+w6xHiTcyU1\/GLfKYZlPiOeuW210Mk5tzud4cRpBeWyExEtGydLhTvUtc+5NUuWwzCvEc9etNjoZp7aMq4nPc9XX10NDQwN0d3cPKSvf1\/sh2Va2v5OJWK59vBIaZ0+KI\/OwzyPEc9fFXonnrpnJSgoTkruY4l6G4yzES6aHPHfJE6\/8B\/8R4CGky61UQrzcGNmmSIV4YY+X5NPVbH3zPPzwxZ4Aqx\/XfQ3qfv8OW9xKMp0Qz123p0I8U3PpXTt1\/6U78QZLMgkp1i4a0kK8aHiFpc4r8Vy8j2cDhU7I0x9eh281\/yLIjrf7Tr\/vVpuiSjqNEM9d9+eVeDbbwFyIqhNSrF10ZIV40TEz5UiFeGFzPBdvoOeCQxWSP5f04rIpMPP+4ruzI5fMPv4uxHOHairEc9fceCWpQs798YnMuh3uTsnXMZt40uQvlxDPHfapEU\/dwIz7LQ8cOGB9Ij2JyKqQ4mbGQ1OIFw83Xa5UiGc6QW5zxs6FqFxIvmAuSwjR0BXiRcMr71HNQnq0BN8ua3hhcO1O3MxoiiTEi4ZX3omHDUDrtnv37syjJRRwwTU8\/nRXVNHUY0G6YA23eDS\/k50qUZEGEOJFxyyvUU2qnF4IunBh8Gla3dm5qKIhoXt7ewPympYniHjczZxZVQ4vfn9K1OpKOr0Qz133pzLHc9fc8JKQeO3t7UPuZSEh+TKCLJpH7xUhXnTM8mrxfO9Q4e5mmKvJH4KX+V10JRLiRccsr8TDynFfZmVl5ZBrGdyJAhB2yxhedkSXF+GFRB0LJ7qsuiTKksuO3HRzqpcdLVmyxPtB2LBbxjrfOgV4yhy\/6ffeCodWTHWDYgmVIhbPXWcX\/RzP9pYxTjyZ38VTICFePNx0uYqeeLbLCfv\/\/Xhwgxh+Mr+Lp0BCvHi4pU48CqoUwp0r31jzYmZ\/5uUfzXSHYAmVJMRz19lFb\/FsoEAhiXiycG6DmD6NEC8+dmrO1IiXz03Sld\/4A\/j48e0SWEmoN0K8hACy7KkQL9+bpDnx1s2aBOtmFdZD9O66029JQjx3+KZCvHxvkubEkxMJ8ZVHiBcfu7y5mr42SdtAgaPL1T9rDZLKUoINYjLHi4+SXc5ULB41xccmaRsxb1vwDPx23P1BUolo2iAmxIuPkl3OVIln1yT3qYh4EtFMhq24msnw47lLgnh01YNsFUumOEK8ZPiVLPEWVd8JLd+tcodeiZUkxHPX4SVl8SSwkkxxhHjJ8CtZiyfES6Y4Qrxk+OWFePxZZd6ANB4toTmeEC+Z4gjxkuGXOvFMB1RziYFrf+vXrw+SmQiqnk7QpSPiyamEXIiH\/12Ilwy\/vBAPX4bdvHkzlJfbXZeOa35NTU3Q0tIS5MET7HhJUnNzM5SVlWVkQFKvWbMGNmzYAJMnT9Yig8STpYTkSiPES44hlZBacIXfBhan+aaLjFSC6soW4sVBfGgeIZ4bHLGUVIjn4mFK050t3B1FgXRXBiLxZA0vudII8ZJjmLrFS9JkW2tpmksi8fC1V9wgLV98BIR48bFTc6Zi8ZI0N8rtZKbLjpB4\/1hzB0y7c2SSppR8XrllzI0KpHbLGDaXRx\/nzJkDa9euhU2bNoUGRWyeara57AiJN+r4T+GrfW+6QU5KEQQcIHDmzBkHpWQXcdPAwMAA\/USkmzBhAtTU1MC+fftg48aNcOjQIejq6hoSqcR8unU\/JCxGNTEffrW1tVmENs3xnEsnBQoCBYpAFvH4Qdj+\/v4M8ZCQUZcZClReaZYgUBAIZBEPW0TrcIsXL4aDBw\/C8uXLYeXKlZD0taCCkFYaIQgUCAJDiKdzH128FlQg8kozBIGCQEBLvLRbxtf4dI+ZpN2eYqmPz69Ng6N6m8CyZcsSvXNYLNi4aqfNjqs4deWdeHxHS09Pj\/b5rjiCDfc8XCFQVr5tj8tu2kk03PFxIR8NWlhWW1ubcatjnLqGEE\/dzIyFUpSS772MU5kuD39fHevOtZ\/TVb3FXg5\/3BP7pbGxEerq6oK5OP9sNzUUOx6u248D27PPPguzZ8+Gp59+Gnbs2OGPeHw5gZ5dpt9QMHXjswthuWLEPR3hoh3FVga3ZNh2JF51dXXW82rqIIrLRK5H7mLDLWp71Queo+Y3pTcuJ\/DTCT4frBTixetKG+KpJZuewI7XgtLIlQrxEErdnCDKdrCo3SGuZlTEBtPbupq8dF9KFE+C4sjlC7MhFs\/0MCXB5PokugRX4imgTXAFXc2tW7fCwoULg\/kJH+R8zNfjSVLYuVIhXr4goOUEmYNE6wG+nMCXYdR9sYsWLQoOJwu+0fDF1MOaeNHhkByCQHEjkPflhOKGT1ovCMRDwHg6gZYTsFjTPSrxqpRcgoAgkPflBOkCQaAUEdCeTujo6MgstNK2Gdy9wq1gKYIlMgsCrhDQ7tW0uZjIVQMKrRzbLVY2t6ZFlY1vaLbdLF5IezEpyup6yYlwpPJ9bmGM2mdx0+d9k3TchvvKl0\/ioWIdPXo0kmdRaMRrb2\/3srWQ+hsHJ7oZoZjXIkuWeHwNjEZoPB0xb968oI\/p+Ix6tQVaoqqqKqCNBpSXNiqjm45fmMXiZdLo3d3dnanbNKJzT4SOARHxRo8eHdSpWhueh6\/jYcDs1KlT8Prrr2euWuTrqTNmzAAsk6YXujariq8OAljHzTffDEeOHAGUz3QkSfUewgYTIZ4vk5NCueoZK77gzC2eunjKd37gTV7qDdrYdFRUVJyGhgbthmRyJ3fu3BmQBDc3IyEon8licGXkpzjwig4cLIh0anmo\/K2trZlbvqmNaqSayzp27NhgYKFbB\/jfJk6cmNVm3l064lG8gMpEOdUTFEK8FJS+EKoIO9wY5mpyxeLEQ5lQUUmp6FSA7piOqpx8z2XYeUTTfll143NY+\/nfsDwiIf6r5uP\/rW41M1kkHfHC6uDuIx\/ExOIVAks8tYEHMrhrpyogKujevXszraC0OuKhO8U\/3alwVYlt9qqa7iHFulQl5e3Xna0kd08lchgR1WAb1qsLoOiIV1lZmTmqZBoUxOJ5UvJCL1Yd3Xt7ezOuH3fVwiye7QFeHxaPu6dhlkq1eGGkMGES1pe5LJ5KbpPFC9vMLXO8QmdTSPvUEdY0x9MdvcFi8UBw2ByPz+N08xnctBx1jqcenyLXFttjQzy0fnzeplo82zkennIw7WTSEQ9\/wzmm6o7z7tHNewlnNYAjxCti4tGcht70464mRe\/QJauvrw8CCRggwAAIPjGGVx7ik2SkSPgvKpYa1Qy7mc0UIcy1NMDdXjWqSbcD6A4WowuMruHSpUvh8OHDwcCxa9cu4BaPY4Jp8frya9euaaOapnU6HfE++eQT6OzsDE5HcEx0c0rsD8QZB4iTJ09qBzghXpETT5pvRiBsThnV1VTJnRR3IV5SBCV\/QSGgrlfGuQaQyiCLiJcFuSQelS87VwpKdaQxgkDxIPD\/\/025a1OGz0AAAAAASUVORK5CYII=","height":134,"width":222}}
 %---
