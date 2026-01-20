5969f168b0745c915d89c279c68e1843ba228d83
dab <dbagnara@gmail.com>
2026-01-20 21:13:59 +0100

    power losses evaluation for FF1200R17

--- power_electronics_design_with_simscape/inv_psm/power_losses_calculus.m
+++ power_electronics_design_with_simscape/inv_psm/power_losses_calculus.m
@@ -4,5 +4,11 @@ clc
 
 ploss_igbtQ1 = mean(igbt_ploss_invQ1_sim)
 ploss_diodeQ1 = mean(diode_ploss_invQ1_sim)
-deltaJH_igbtQ1 = mean(igbt_HA_temp_invQ1_sim)
+deltaJH_igbtQ1 = mean(igbt_JH_temp_invQ1_sim)
 deltaJH_diodeQ1 = mean(diode_JH_temp_invQ1_sim)
+
+rippleJH_igbtQ1 = max(igbt_JH_temp_invQ1_sim) - mean(igbt_JH_temp_invQ1_sim)
+rippleJH_diodeQ1 = max(diode_JH_temp_invQ1_sim) - mean(diode_JH_temp_invQ1_sim)
+
+TjMAX_igbt = 65 + rippleJH_igbtQ1 + deltaJH_igbtQ1
+TjMAX_diode = 65 + rippleJH_diodeQ1 + deltaJH_diodeQ1
