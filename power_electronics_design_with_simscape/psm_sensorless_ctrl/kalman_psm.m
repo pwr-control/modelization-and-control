de542e216d26d63453842535323b6d4b8f6a4929
dab <dbagnara@gmail.com>
2026-02-09 14:04:51 +0100

    working on EKF BEMF for psm

---
+++ power_electronics_design_with_simscape/psm_sensorless_ctrl/kalman_psm.m
@@ -0,0 +1,40 @@
+clc
+
+%% PSM paramerters
+
+A1_tilde_ekf = [-Rs_norm/Lalpha_norm 0 0 0 0 0; 0 -Rs_norm/Lalpha_norm 0 0 0 0; ...
+    0 0 0 0 0 0; 0 0 0 0 0 0; ...
+    0 0 0 0 0 1; 0 0 0 0 0 0];
+
+A2_tilde_ekf = [0 0 0 1/Lbeta_norm 0 0; 0 0 -1/Lbeta_norm 0 0 0; ...
+    0 0 0 -1 0 0; 0 0 1 0 0 0;...
+    0 0 0 0 0 0; 0 0 0 0 0 0];
+
+A3_tilde_ekf = [0 0 0 0 0 0; 0 0 0 0 0 -1/Lbeta_norm; ...
+    0 0 0 0 0 0; 0 0 0 0 0 1;...
+    0 0 0 0 0 0; 0 0 0 0 0 0];
+
+A4_tilde_ekf = [0 0 0 0 0 1/Lalpha_norm; 0 0 0 0 0 0; ...
+    0 0 0 0 0 -1; 0 0 0 0 0 0;...
+    0 0 0 0 0 0; 0 0 0 0 0 0];
+
+B_tilde_ekf = [1/Lalpha_norm 0; 0 1/Lbeta_norm; 0 0; 0 0; 0 0; 0 0];
+
+C_ekf = [1 0 0 0 0 0; 0 1 0 0 0 0];
+Bd_ekf = B_tilde_ekf*ts_inv;
+
+%% Kalman init
+Qkalman = [Rs_norm/Lalpha_norm 0 0 0 0 0; 0 Rs_norm/Lbeta_norm 0 0 0 0; ...
+    0 0 2*ts_inv 0 0 0;...
+    0 0 0 2*ts_inv 0 0;...
+    0 0 0 0 1 0; 0 0 0 0 0 1];
+Rkalman = [1 0; 0 1];
+
+
+
+
+
+
+
+
+
