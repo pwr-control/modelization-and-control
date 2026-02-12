a5a5b89275945edf41fc0fbec21831847d05d602
dab <dbagnara@gmail.com>
2026-02-12 12:33:26 +0100

    new dt gen with min pulse

--- semiconductor_device_modeling/circuits/deadtime_circuit_generator/init_dtg.m
+++ semiconductor_device_modeling/circuits/deadtime_circuit_generator/init_dtg.m
@@ -1,21 +1,15 @@
 preamble;
 
-simlength = 1e-3;
 
 tc = 1e-7;
 
-dt = 0.1e-3;
+Ton_base = 1e-6;
+dT_base = 0.1e-3;
+N = 10;
 
-vin_cmd = 12;
-t1 = dt;
-Ton1 = 2e-6;
-t2 = t1 + dt;
-Ton2 = 4e-6;
-t3 = t2 + dt;
-Ton3 = 6e-6;
-t4 = t3 + dt;
-Ton4 = 8e-6;
-t5 = t4 + dt;
-Ton5 = 10e-6;
+vin_cmd = 15;
+vout_cmd = 15;
 
-open_system dtg.slx;
+simlength = dT_base + N*dT_base;
+
+open_system dtg_w.slx;
