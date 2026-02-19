9ad589d59755d1ab70793a24adffa0eae3b17613
dab <dbagnara@gmail.com>
2026-02-19 18:33:46 +0100

    no message

--- power_electronics_design_with_simscape/im_analysis_and_design/im_calculus.m
+++ power_electronics_design_with_simscape/im_analysis_and_design/im_calculus.m
@@ -15,8 +15,8 @@ function im = im_calculus()
     u_cc = u_nom;
     i_cc = 3084;
     f_cc = freq_nom;
-    Jm = 40;
-    load_friction_factor = 0.3;
+    Jm = 5;
+    load_friction_factor = 0.75;
     im = im_setup('GM355L6-250kW', pwr_nom, u_nom, i_nom, freq_nom, rpm_load, number_poles, eta, cosphi, ...
                             Rs, u_no_load, i_no_load, f_no_load, u_cc, i_cc, f_cc, Jm, load_friction_factor);
     displayInfo(im);
